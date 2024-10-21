3fed8866f11bd68c165d140c8e9a38f9fad7e174
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2024-10-21 12:54:38 +0200

    py math operators

---
+++ Python/operators.py
@@ -0,0 +1,20 @@
+x = 1
+y = 2
+
+if x == y:
+    print("They're equal")
+    print(x,y)
+else:
+    print("They're not equal")
+    print(x,y)
+
+val1 = 1_000_000
+print(val1)
+
+# math
+val1 = 1/2
+val2 = 10//3    # return int from division
+val3 = 10%2     # return if odd/rest (1) or even/no-rest (0)
+val4 = 2**10    # 2^10
+
+print(val3)
