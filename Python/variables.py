7e5bfca8f6596b6d8e8eb5d223ce451fe306ac95
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2025-03-06 15:26:51 +0100

    update python

---
+++ Python/variables.py
@@ -0,0 +1,14 @@
+# hours_rate = 24
+
+# def time_converter(minutes_rate):
+#     #minutes_rate = 3
+#     print(f"Hours number is {hours_rate} * {minutes_rate} minutes = {hours_rate * minutes_rate}")
+
+# #time_converter(5)
+
+imie = input("Podaj imie: ")
+
+def hello(minutes_rate):
+    print(f"Witaj {imie}!")
+    
+hello(imie)
--- Python/variables2.py
+++
@@ -1,7 +0,0 @@
-hours_rate = 24
-
-def time_converter(minutes_rate):
-    #minutes_rate = 3
-    print(f"Hours number is {hours_rate} * {minutes_rate} minutes = {hours_rate * minutes_rate}")
-
-time_converter(3)
